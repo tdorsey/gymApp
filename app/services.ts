1e4ae3358e7733c7c00a795e5538f5c4102f9f90
Tommy Dorsey <tdorsey@gmail.com>
2017-04-19 13:37:51 -0400

    Implement IExercise

--- app/services.ts
+++ app/services.ts
@@ -17,10 +17,11 @@ module Services {
     id: Number;
     name: String;
     workingWeight: Number;
-    warmupWeight: Number;
+    warmupWeight?: Number;
     workingSets: Number;
     warmupSets: Number;
-    maxWeight: Number;
+    maxWeight?: Number;
+    maxReps?: Array<Number>;
     reps: Number;
   }
 
@@ -107,8 +108,114 @@ module Services {
       return null;
     }
   }
+
+  export class Exercises implements IExercise {
+  id: Number;
+    name: String;
+    workingWeight: Number;
+    warmupWeight?: Number;
+    workingSets: Number;
+    warmupSets: Number;
+    maxWeight?: Number;
+    maxReps?: Array<Number>;
+    reps: Number;
+    exercises: Array<IExercise>;
+     constructor(IUserSettings) {
+       this.exercises = [
+         {
+     id: 0,
+    name: "Squat",
+    workingWeight: 10,
+    warmupWeight: 10,
+    workingSets: 5,
+    warmupSets: 0,
+    maxWeight: 10,
+    reps: 5
+      },
+      {
+     id: 1,
+    name: "Overhead Press",
+    workingWeight: 55,
+    warmupWeight: 45,
+    workingSets: 3,
+    warmupSets: 2,
+    maxWeight: 55,
+    reps: 5
+      },    {
+     id: 2,
+    name: "Deadlift",
+    workingWeight: 115,
+    warmupWeight: null,
+    workingSets: 1,
+    warmupSets: 0,
+    maxWeight: 110,
+    reps: 5
+      },    {
+     id: 3,
+    name: "Overhand Pullup",
+    workingWeight: 130,
+    warmupWeight: 0,
+    workingSets: 3,
+    warmupSets: 0,
+    maxWeight: null,
+    maxReps: [8,7,7],
+    reps: 3
+      },    {
+     id: 4,
+    name: "Underhand Pullup",
+    workingWeight: 130,
+    warmupWeight: 0,
+    workingSets: 3,
+    warmupSets: 0,
+    maxWeight: null,
+    maxReps: [8,7,7],
+    reps: 3
+      },
+     {
+     id: 5,
+    name: "Bench Press",
+    workingWeight: 70,
+    warmupWeight: 0,
+    workingSets: 3,
+    warmupSets: 2,
+    maxWeight: 70,
+    maxReps: null,
+    reps: 5
+      },
+  {
+     id: 6,
+    name: "Sit Ups",
+    workingWeight: 0,
+    warmupWeight: 0,
+    workingSets: 3,
+    warmupSets: 0,
+    maxWeight: 0,
+    maxReps: [11,7,4],
+    reps: 3
+      },
+      
+      ];
+    }
+    all() {
+      console.log(this.exercises);
+      return this.exercises;
+    };
+    remove(exercise) {
+      this.exercises.splice(this.exercises.indexOf(exercise), 1);
+    };
+    get(exerciseId) {
+      for (var i = 0; i < this.exercises.length; i++) {
+        if (this.exercises[i].id === parseInt(exerciseId)) {
+          return this.exercises[i];
+        }
+      }
+      return null;
+    }
+  }
+
 }
 
 angular.module('starter.services', [])
   .service('Settings', Services.UserSettings)
+    .service('Exercises', Services.Exercises)
   .service('Weights', Services.Weights);
